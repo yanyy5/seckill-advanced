e3c2ddc91716008921b601cad8c869be352d3719
yanyy5 <okyangyang97@gmail.com>
2021-11-24 21:20:35 +0800

    identify some methods in EmBusinessError.java

--- src/main/java/com/seckill/error/EmBusinessError.java
+++ src/main/java/com/seckill/error/EmBusinessError.java
@@ -1,4 +1,35 @@
 package com.seckill.error;
 
-public enum EmBusinessError {
+public enum EmBusinessError implements CommonError{
+
+    // common error type: 00001
+    PARAMETER_VALIDATION_ERROR(00001, "invalid para"),
+
+    // start from 10000: about user info error
+    USER_NOT_EXIST(10001, "user not exists"),
+    ;
+
+    private int errCode;
+    private String errMsg;
+
+    private EmBusinessError(int errCode, String errMsg) {
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    @Override
+    public int getErrCode() {
+        return this.errCode;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return this.errMsg;
+    }
+
+    @Override
+    public CommonError setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+        return this;
+    }
 }
