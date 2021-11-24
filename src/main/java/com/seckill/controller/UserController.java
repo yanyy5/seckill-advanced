1d1ceb45d402e8a15333865e94890e8a89e3918f
yanyy5 <okyangyang97@gmail.com>
2021-11-24 16:25:40 +0800

    update getUser method

--- src/main/java/com/seckill/controller/UserController.java
+++ src/main/java/com/seckill/controller/UserController.java
@@ -1,16 +1,29 @@
 package com.seckill.controller;
 
+import com.seckill.service.UserService;
+import com.seckill.service.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("user")
 @RequestMapping("/user")
 public class UserController {
 
-    public void getUser(@RequestParam(name="id") Integer id){
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public UserModel getUser(@RequestParam(name="id") Integer id){
         // use service to get the User object with the related id
         // and return it to front-end
+
+        UserModel userModel = userService.getUserById(id);
+
+        return userModel;
 
     }
 
