b2946b2e15ef5404ca5f665f08ca1b30909a394e
yanyy5 <okyangyang97@gmail.com>
2021-11-24 17:02:26 +0800

    1.change the return type of getUser from UserModel to UserVO. 2.add convertFromModel method

--- src/main/java/com/seckill/controller/UserController.java
+++ src/main/java/com/seckill/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.seckill.controller;
 
+import com.seckill.controller.viewObject.UserVO;
 import com.seckill.service.UserService;
 import com.seckill.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +19,25 @@ public class UserController {
 
     @RequestMapping("/get")
     @ResponseBody
-    public UserModel getUser(@RequestParam(name="id") Integer id){
+    public UserVO getUser(@RequestParam(name="id") Integer id){
         // use service to get the User object with the related id
         // and return it to front-end
 
         UserModel userModel = userService.getUserById(id);
 
-        return userModel;
+        // convert the userModel(with all the info) to userVO(the viewObject that can be used by UI)
+        return convertFromModel(userModel);
 
+    }
+
+    private UserVO convertFromModel(UserModel userModel){
+        if (userModel == null){
+            return null;
+        }
+
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(userModel, userVO);
+        return userVO;
     }
 
 }
