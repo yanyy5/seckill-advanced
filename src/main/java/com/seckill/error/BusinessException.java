e5f56a73b611e28e5b07c9cf6166f408cf164654
yanyy5 <okyangyang97@gmail.com>
2021-11-25 12:29:51 +0800

    Add 3 methods in BusinessException.java

--- src/main/java/com/seckill/error/BusinessException.java
+++ src/main/java/com/seckill/error/BusinessException.java
@@ -1,18 +1,36 @@
 package com.seckill.error;
 
+// Decorator pattern - BusinessException
 public class BusinessException extends Exception implements CommonError{
+
+    private CommonError commonError;
+
+    // receive para of EmBusinessError for exceptions
+    public BusinessException(CommonError commonError){
+        super();
+        this.commonError = commonError;
+    }
+
+    // DIY errMsg
+    public BusinessException(CommonError commonError, String errMsg){
+        super();
+        this.commonError = commonError;
+        this.commonError.setErrMsg(errMsg);
+    }
+
     @Override
     public int getErrCode() {
-        return 0;
+        return this.commonError.getErrCode();
     }
 
     @Override
     public String getErrMsg() {
-        return null;
+        return this.commonError.getErrMsg();
     }
 
     @Override
     public CommonError setErrMsg(String errMsg) {
-        return null;
+        this.commonError.setErrMsg(errMsg);
+        return this;
     }
 }
