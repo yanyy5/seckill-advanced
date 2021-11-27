f8dbf0c10e83c14e2754aebf1cf07eff3986c4cc
yanyy5 <okyangyang97@gmail.com>
2021-11-27 14:30:04 +0800

    Add getOtp method and set the requestMethod as POST

--- src/main/java/com/seckill/controller/UserController.java
+++ src/main/java/com/seckill/controller/UserController.java
@@ -16,13 +16,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Controller("user")
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    // interface: for a user to get otp messages
+    @RequestMapping(value = "/getotp", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "phone") String phone){
+        // generate OTP valid code by some rules
+        // random number
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String optCode = String.valueOf(randomInt);
+
+        // bing the OTP code with phone number
+        // (key, value) redis
+        // HTTP session
+        httpServletRequest.getSession().setAttribute(phone, optCode);
+
+        // send the OTP code (message) to users (skip)
+        System.out.println("phone = " + phone + "& optCode = " + optCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
