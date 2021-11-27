c216fe4537f88c2c20703f1700fb285cae61160e
yanyy5 <okyangyang97@gmail.com>
2021-11-27 20:44:48 +0800

    add register method

--- src/main/java/com/seckill/service/UserService.java
+++ src/main/java/com/seckill/service/UserService.java
@@ -1,10 +1,13 @@
 package com.seckill.service;
 
+import com.seckill.error.BusinessException;
 import com.seckill.service.model.UserModel;
 
 public interface UserService {
 
     // to get user object by user id
     UserModel getUserById(Integer id);
+
+    void register(UserModel userModel) throws BusinessException;
 
 }
