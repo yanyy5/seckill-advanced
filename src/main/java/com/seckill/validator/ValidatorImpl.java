0defe8af3ff764ef88885c94f56a3dfc4d7b4c10
yanyy5 <okyangyang97@gmail.com>
2021-11-28 12:41:08 +0800

    add validate method and implement afterPropertiesSet method

--- src/main/java/com/seckill/validator/ValidatorImpl.java
+++ src/main/java/com/seckill/validator/ValidatorImpl.java
@@ -1,4 +1,39 @@
 package com.seckill.validator;
 
-public class ValidatorImpl {
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Component
+public class ValidatorImpl implements InitializingBean {
+
+    private Validator validator;
+
+    // implement validation method and return the result
+    public ValidationResult validate(Object bean) {
+        final ValidationResult result = new ValidationResult();
+        Set<ConstraintViolation<Object>> constraintViolationSet = validator.validate(bean);
+        if (constraintViolationSet.size() > 0){
+            // has errors
+            result.setHasErrors(true);
+            constraintViolationSet.forEach(constraintViolation -> {
+                String errMsg = constraintViolation.getMessage();
+                String propertyName = constraintViolation.getPropertyPath().toString();
+                result.getErrMsgMap().put(propertyName, errMsg);
+            });
+        }
+        return result;
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        // instancing hibernate validator by factory initialization
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
 }
