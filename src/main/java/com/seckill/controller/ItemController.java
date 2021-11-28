efc3fd9c521271e7f3d678904697c22f6fa574c5
yanyy5 <okyangyang97@gmail.com>
2021-11-28 20:38:16 +0800

    add createItem method

--- src/main/java/com/seckill/controller/ItemController.java
+++ src/main/java/com/seckill/controller/ItemController.java
@@ -1,4 +1,58 @@
 package com.seckill.controller;
 
-public class ItemController {
+import com.seckill.controller.viewObject.ItemVO;
+import com.seckill.error.BusinessException;
+import com.seckill.response.CommonReturnType;
+import com.seckill.service.ItemService;
+import com.seckill.service.model.ItemModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@Controller("item")
+@RequestMapping("/item")
+@CrossOrigin(allowCredentials = "true", allowedHeaders = "*")
+public class ItemController extends BaseController{
+
+    @Autowired
+    private ItemService itemService;
+
+    // controller: create item
+    @RequestMapping(value = "/create", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType createItem(@RequestParam(name = "title")String title,
+                                       @RequestParam(name = "description") String description,
+                                       @RequestParam(name = "price") BigDecimal price,
+                                       @RequestParam(name = "stock") Integer stock,
+                                       @RequestParam(name = "imgUrl")String imgUrl) throws BusinessException {
+
+        // create item with service
+        ItemModel itemModel = new ItemModel();
+        itemModel.setTitle(title);
+        itemModel.setDescription(description);
+        itemModel.setPrice(price);
+        itemModel.setStock(stock);
+        itemModel.setImgUrl(imgUrl);
+
+        ItemModel returnItemModel = itemService.createItem(itemModel);
+
+        // return information to frond-end: itemVO
+        ItemVO itemVO = convertItemVOFromModel(returnItemModel);
+
+        return CommonReturnType.create(itemVO);
+
+    }
+
+    private ItemVO convertItemVOFromModel(ItemModel itemModel){
+        if (itemModel == null){
+            return null;
+        }
+        ItemVO itemVO = new ItemVO();
+        BeanUtils.copyProperties(itemModel, itemVO);
+        return itemVO;
+    }
+
 }
