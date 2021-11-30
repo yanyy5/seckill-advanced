53c5e551ba7cf9458becdd9ef47f875c00465924
yanyy5 <okyangyang97@gmail.com>
2021-11-30 15:42:25 +0800

    add variables and getter/setter in PromoModel.java

--- src/main/java/com/seckill/service/model/PromoModel.java
+++ src/main/java/com/seckill/service/model/PromoModel.java
@@ -1,4 +1,59 @@
 package com.seckill.service.model;
 
+
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+
 public class PromoModel {
+
+    private Integer id;
+
+    private String promoName;
+
+    private DateTime startDate;
+
+    private Integer itemId;
+
+    private BigDecimal promoPrice;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPromoName() {
+        return promoName;
+    }
+
+    public void setPromoName(String promoName) {
+        this.promoName = promoName;
+    }
+
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public BigDecimal getPromoPrice() {
+        return promoPrice;
+    }
+
+    public void setPromoPrice(BigDecimal promoPrice) {
+        this.promoPrice = promoPrice;
+    }
 }
