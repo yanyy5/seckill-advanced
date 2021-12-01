66f462ca66aeb29b1a5c4a27851552a32e0eb708
yanyy5 <okyangyang97@gmail.com>
2021-12-01 10:04:58 +0800

    add promoId as @RequestParam

--- src/main/java/com/seckill/controller/OrderController.java
+++ src/main/java/com/seckill/controller/OrderController.java
@@ -29,7 +29,9 @@ public class OrderController extends BaseController{
     @RequestMapping(value = "/create", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
     @ResponseBody
     public CommonReturnType createOrder(@RequestParam(name = "itemId")Integer itemId,
-                                        @RequestParam(name = "amount")Integer amount) throws BusinessException {
+                                        @RequestParam(name = "amount")Integer amount,
+                                        @RequestParam(name = "promoId", required = false)Integer promoId
+                                        ) throws BusinessException {
 
         Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
         if (isLogin == null || !isLogin){
@@ -38,7 +40,7 @@ public class OrderController extends BaseController{
 
         // get user login info
         UserModel userModel = (UserModel) httpServletRequest.getSession().getAttribute("LOGIN_USER");
-        OrderModel orderModel = orderService.createOrder(userModel.getId(), itemId, amount);
+        OrderModel orderModel = orderService.createOrder(userModel.getId(), itemId, promoId, amount);
 
         return CommonReturnType.create(null);
     }
