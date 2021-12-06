a96083b87933712f377084b8528be40caec531aa
yanyy5 <okyangyang97@gmail.com>
2021-12-06 15:49:11 +0800

    handle 404, 405 error in GlobalExceptionHandler

--- src/main/java/com/seckill/controller/BaseController.java
+++ src/main/java/com/seckill/controller/BaseController.java
@@ -17,27 +17,27 @@ public class BaseController {
     public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
 
     // identify exception handler to fix the exception not in controller layer
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception e){
-
-        Map<String, Object> responseData = new HashMap<>();
-
-        // not all the exception are businessException
-        if (e instanceof BusinessException){
-            BusinessException businessException = (BusinessException)e;
-            responseData.put("errCode", businessException.getErrCode());
-            responseData.put("errMsg", businessException.getErrMsg());
-        }
-        else{
-            // identify a common exception msg
-            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
-            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
-        }
-
-        return CommonReturnType.create(responseData, "fail");
-
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Object handlerException(HttpServletRequest request, Exception e){
+//
+//        Map<String, Object> responseData = new HashMap<>();
+//
+//        // not all the exception are businessException
+//        if (e instanceof BusinessException){
+//            BusinessException businessException = (BusinessException)e;
+//            responseData.put("errCode", businessException.getErrCode());
+//            responseData.put("errMsg", businessException.getErrMsg());
+//        }
+//        else{
+//            // identify a common exception msg
+//            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+//            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+//        }
+//
+//        return CommonReturnType.create(responseData, "fail");
+//
+//    }
 
 }
--- src/main/java/com/seckill/controller/GlobalExceptionHandler.java
+++ src/main/java/com/seckill/controller/GlobalExceptionHandler.java
@@ -1,4 +1,48 @@
 package com.seckill.controller;
 
+import com.seckill.error.BusinessException;
+import com.seckill.error.EmBusinessError;
+import com.seckill.response.CommonReturnType;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    // handle 404 and 405 error
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public CommonReturnType doError(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Exception ex){
+        ex.printStackTrace();
+        Map<String, Object> responseData = new HashMap<>();
+
+        // not all the exception are businessException
+        if (ex instanceof BusinessException){
+            BusinessException businessException = (BusinessException)ex;
+            responseData.put("errCode", businessException.getErrCode());
+            responseData.put("errMsg", businessException.getErrMsg());
+        }
+        else if (ex instanceof ServletRequestBindingException){
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", "url binding router problem");
+        }
+        else if (ex instanceof NoHandlerFoundException){
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", "cannot find the visiting path");
+        }
+        else{
+            // identify a common exception msg
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+        }
+
+        return CommonReturnType.create(responseData, "fail");
+    }
 }
