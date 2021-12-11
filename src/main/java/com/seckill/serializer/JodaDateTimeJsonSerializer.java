565cce25161ffa106fbec92c0fcf6c42ee2079f0
yanyy5 <okyangyang97@gmail.com>
2021-12-11 22:06:00 +0800

    Serializer/Deserializer for JodaDateTime

--- src/main/java/com/seckill/serializer/JodaDateTimeJsonSerializer.java
+++ src/main/java/com/seckill/serializer/JodaDateTimeJsonSerializer.java
@@ -1,4 +1,15 @@
 package com.seckill.serializer;
 
-public class JodaDateTimeJsonSerializer {
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+
+public class JodaDateTimeJsonSerializer extends JsonSerializer<DateTime> {
+    @Override
+    public void serialize(DateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeString(value.toString("yyyy-MM-dd HH:mm:ss"));
+    }
 }
